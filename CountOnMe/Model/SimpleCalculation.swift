696141e2d0754f62cd5308e557dcaabe13c77b6d
Agrebaoui Jihed <jihed.agrebaoui@expleogroup.com>
2021-06-18 16:27:25 +0200

    CountOnME_18062021

--- CountOnMe/Model/SimpleCalculation.swift
+++ CountOnMe/Model/SimpleCalculation.swift
@@ -60,7 +60,8 @@ class SimpleCalculation {
     }
    
     func calcultation(_ firstNumber: Float, _ secondNumber: Float,_ operand: String) throws -> Float {
-    
+     
+       
         switch operand {
         case "+":
             operationType = .addition
@@ -86,12 +87,12 @@ class SimpleCalculation {
         default:
             break
           }
+        
         return 0.0
         }
     func add(number: String) {
         if expressionHaveResult {
-            guard let lastElement = elements.last else { return }
-            text = lastElement
+            text = ""
         }
         text.append(number)
         }
@@ -110,19 +111,29 @@ class SimpleCalculation {
     
     func addresult() {
         if expressionIsCorrect && expressionHaveEnoughElement {
-            var operationsToReduce = elements
-            while operationsToReduce?.count ?? 0 > 1 {
-                
-                let firstNumber = Float(operationsToReduce![0])
-                let secondNumber = Float(operationsToReduce![2])
-                let operand = operationsToReduce![1]
+            var operationsToReduce = elements!
+    
+            while operationsToReduce.count > 1 {
+                if let index = operationsToReduce.firstIndex(where : { $0 == "x" || $0 == "/" }) {
+                    let leftValue = Float(operationsToReduce[index - 1])!
+                    let operand = operationsToReduce[index]
+                    let rightValue = Float(operationsToReduce[index + 1])!
+                    let result = try? calcultation(leftValue, rightValue, operand)
+                    operationsToReduce[index] = String(result!)
+                    operationsToReduce.remove(at: index + 1)
+                    operationsToReduce.remove(at: index - 1)
+                } else {
+                let firstNumber = Float(operationsToReduce[0])
+                let secondNumber = Float(operationsToReduce[2])
+                let operand = operationsToReduce[1]
                 let result = try? calcultation(firstNumber!, secondNumber! , operand)
                 
-                operationsToReduce = Array(operationsToReduce!.dropFirst(3))
-                operationsToReduce?.insert(String(result!), at: 0)
-                
+                operationsToReduce = Array(operationsToReduce.dropFirst(3))
+                operationsToReduce.insert(String(result!), at: 0)
+                }
             }
-            delegate?.didResult(operation: text + " = \(operationsToReduce?.first ?? "")")
+            text = text + " = \(operationsToReduce.first ?? "")"
+                        delegate?.didResult(operation: text)
             
         } else  {
             delegate?.alertExpression()
